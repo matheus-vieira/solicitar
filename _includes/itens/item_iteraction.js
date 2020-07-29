8cafbbc024ad26b3bf7f5aba7189e9f31941f531
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2020-07-29 09:50:07 -0300

    fixed remove itens featured, correct remove itens

--- _includes/itens/item_iteraction.js
+++ _includes/itens/item_iteraction.js
@@ -6,19 +6,31 @@ const addItem = function addItem() {
     const item = new Item({});
     addHtml(divItens, itemTpl, item);
     itens.push(item);
+    checkRemoveBtn();
+  },
+  checkRemoveBtn = function () {
+    document.querySelectorAll(".remove-item").forEach((btn) => {
+      if (itens.length == 1) btn.classList.add("d-none");
+      else btn.classList.remove("d-none");
+    });
   },
   removeItem = function removeItem(id) {
-    const elem = document.getElementById(`fieldset-${id}`);
+    const elem = document.getElementById(`${id}`);
     if (elem) {
       divItens.removeChild(elem);
     }
-    !divItens.childEle69.mentCount && addItem();
+    itensValid = itens.filter((i) => i.id != id);
+    itens.length = 0;
+    Array.prototype.push.apply(itens, itensValid);
+    !divItens.childElementCount && addItem();
+    checkRemoveBtn();
   },
   loadItens = function loadItens() {
     const selectedItens = solicitacao.getItens();
     if (selectedItens) {
       Array.prototype.push.apply(itens, selectedItens);
-      itens.forEach(i => addHtml(divItens, itemTpl, i));
+      itens.forEach((i) => addHtml(divItens, itemTpl, i));
+      checkRemoveBtn();
       return;
     }
     addItem();
